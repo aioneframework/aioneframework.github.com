4de9d77d098b93b1546147acea4317bbbb0d4109
SGS Sandhu <sgs.sandhu@gmail.com>
2017-12-14 17:00:30 +0530

    Update 20171214113030

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#aione_header').load('template/header.html');
+    $('#aione_footer').load('template/footer.html');
+    $('#aione_copyright').load('template/copyright.html');
+});
